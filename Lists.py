79972764ea9d76fbd3a0d0a18aa6a4ce319a759e
Randall <quaceyrandy@gmail.com>
2025-01-30 15:38:21 +0000

    Learn Python Lists

---
+++ Lists.py
@@ -0,0 +1,55 @@
+#Lists
+# cars = ['mercedes', 'toyota', 'porsche', 'lamborghini']
+
+# cars.insert(5, 'ferrari')
+# cars.append('lexus')
+# print(cars)
+# cars.sort()
+# cars.remove('toyota')
+# print('ferrari' in cars)
+# print(len(cars))
+# print(cars)
+# cars.clear()
+# To see the attributes and methods associated 
+# print(dir(cars))
+# To see the usage of attributes and methods
+# print(help(cars))
+
+#Sets
+
+# cars = {'mercedes', 'toyota', 'porsche', 'lamborghini'}
+
+# cars.add('ferrari')
+# print(len(cars))
+# print('ferrari' in cars)
+# print(cars)
+# cars.remove('toyota')
+# print(cars)
+# cars.pop()
+# print(cars)
+# cars.clear()
+# print(cars)
+# To see the attributes and methods associated 
+# print(dir(cars))
+# To see the usage of attributes and methods
+# print(help(cars))
+
+
+#Tuples
+
+cars = ('mercedes', 'toyota', 'porsche', 'lamborghini', 'porsche')
+
+
+print(len(cars))
+print('ferrari' in cars)
+print(cars.count('porsche'))
+print(cars.index('lamborghini'))
+# print(cars)
+# cars.pop()
+# print(cars)
+# cars.clear()
+print(cars)
+# To see the attributes and methods associated 
+# print(dir(cars))
+# To see the usage of attributes and methods
+# print(help(cars))
